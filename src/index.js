65a3150823a9c830e864463f6488373fd11e9eef
BetaHuhn <51766171+BetaHuhn@users.noreply.github.com>
2020-10-28 19:45:58 +0100

    Change default email theme to ackee

--- src/index.js
+++ src/index.js
@@ -5,52 +5,52 @@ const packageJson = require('../package.json')
 const Runner = require('./Runner')
 
 program
-	.version(packageJson.version, '-v, --version')
+    .version(packageJson.version, '-v, --version')
 
 program
-	.command('email')
-	.description('Generate report and send it via email')
-	.option('-d, --domain <titles...>', 'specify domains by title')
-	.option('-i, --id <ids...>', 'specify domains by id')
-	.option('-t, --to <recipient...>', 'to whom the report should be sent')
-	.option('-s, --style <name>', 'email style to use', 'basic')
-	.action((args, program) => {
-		const runner = new Runner(args, program)
-		runner.email()
-	})
+    .command('email')
+    .description('Generate report and send it via email')
+    .option('-d, --domain <titles...>', 'specify domains by title')
+    .option('-i, --id <ids...>', 'specify domains by id')
+    .option('-t, --to <recipient...>', 'to whom the report should be sent')
+    .option('-s, --style <name>', 'email style to use', 'ackee')
+    .action((args, program) => {
+        const runner = new Runner(args, program)
+        runner.email()
+    })
 
 program
-	.command('json')
-	.description('Query API for data and output it to JSON file')
-	.option('-d, --domain <titles...>', 'specify domains by title')
-	.option('-i, --id <ids...>', 'specify domains by id')
-	.option('-o, --output <file>', 'path to output file', 'report.json')
-	.action((args, program) => {
-		const runner = new Runner(args, program)
-		runner.json()
-	})
+    .command('json')
+    .description('Query API for data and output it to JSON file')
+    .option('-d, --domain <titles...>', 'specify domains by title')
+    .option('-i, --id <ids...>', 'specify domains by id')
+    .option('-o, --output <file>', 'path to output file', 'report.json')
+    .action((args, program) => {
+        const runner = new Runner(args, program)
+        runner.json()
+    })
 
 program
-	.command('domains [titles...]')
-	.alias('domain')
-	.description('get domain id by title')
-	.action((args, program) => {
-		const runner = new Runner(args, program)
-		runner.domains()
-	})
+    .command('domains [titles...]')
+    .alias('domain')
+    .description('get domain id by title')
+    .action((args, program) => {
+        const runner = new Runner(args, program)
+        runner.domains()
+    })
 
 program
-	.command('config')
-	.description('output current config')
-	.action((args, program) => {
-		const runner = new Runner(args, program)
-		runner.outputConfig()
-	})
+    .command('config')
+    .description('output current config')
+    .action((args, program) => {
+        const runner = new Runner(args, program)
+        runner.outputConfig()
+    })
 
 program.on('command:*', (operands) => {
-	console.error(`error: unknown command '${ operands[0] }'\n`)
-	program.help()
-	process.exitCode = 1
+    console.error(`error: unknown command '${ operands[0] }'\n`)
+    program.help()
+    process.exitCode = 1
 })
 
 program.parse(process.argv)
