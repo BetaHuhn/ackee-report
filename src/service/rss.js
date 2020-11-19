fa9a10ebc3c9ec87a0a2ae545b23606ba9aaba44
BetaHuhn <51766171+BetaHuhn@users.noreply.github.com>
2020-11-19 18:53:37 +0100

    Generate rss feed

--- src/service/rss.js
+++ src/service/rss.js
@@ -1,8 +1,46 @@
 const fs = require('fs')
+const { Feed } = require('feed')
 
-const report = async function(data, output) {
+const items = [ 'pages', 'referrers', 'languages', 'browsers', 'devices', 'sizes', 'systems' ]
+
+const capitalize = (string) => string.charAt(0).toUpperCase() + string.slice(1)
+
+const report = async function(data, config, output) {
 	return new Promise((resolve, reject) => {
-		const result = JSON.stringify(data, null, 2)
+		const feed = new Feed({
+			title: 'Ackee Report',
+			description: `Here is your report for ${ data.namesShort }`,
+			id: config.ackee.server,
+			link: config.ackee.server,
+			language: 'en',
+			favicon: `${ config.ackee.server }/favicon.ico`,
+			generator: 'ackee-report'
+		})
+
+		data.domains.forEach((domain) => {
+			let content = `<p><b>Average views per day</b></p><p>${ domain.viewsAvgDay }</p><p><b>Average duration</b></p><p>${ domain.durationAvg }s</p><p><b>Views this month</b></p><p>${ domain.viewsMonth }</p>`
+
+			for (const idx in items) {
+				const field = domain[items[idx]]
+				if (field === undefined || field.length < 1) break
+
+				let list = `<p><b>Top ${ capitalize(items[idx]) }</b></p>`
+				field.forEach((item) => {
+					list += (`<p>${ item.id } - ${ item.count } times</p>`)
+				})
+
+				content += list
+			}
+
+			feed.addItem({
+				title: domain.title,
+				description: content,
+				link: config.ackee.server,
+				date: new Date()
+			})
+		})
+
+		const result = feed.rss2()
 
 		fs.writeFile(output, result, (err) => {
 			if (err) reject(err)
