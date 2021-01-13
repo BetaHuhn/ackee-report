b5ecb58433a0cf6619c9004224be79f8dcabde35
BetaHuhn <51766171+BetaHuhn@users.noreply.github.com>
2021-01-13 21:47:38 +0100

    ✨ Add support for env variables as config

--- src/Config.js
+++ src/Config.js
@@ -2,13 +2,26 @@ const Configstore = require('configstore')
 const prompt = require('prompt-sync')({ sigint: true })
 const packageJson = require('../package.json')
 
+require('dotenv').config()
+
 const config = new Configstore(packageJson.name, {})
 
 const fields = [ 'ackee.server', 'email.host', 'email.port', 'email.username', 'email.password', 'email.from' ]
 
+const getEnv = (key) => {
+	const envKey = key.split('.').join('_').toUpperCase()
+	return process.env[envKey]
+}
+
 const verifyField = (field) => {
 	const existing = config.get(field)
 	if (existing) return existing
+
+	const env = getEnv(field)
+	if (env !== undefined) {
+		config.set(field, env)
+		return
+	}
 
 	const text = field.split('.').join(' ')
 	const required = !field.includes('email')
@@ -22,8 +35,17 @@ const verifyField = (field) => {
 	return value
 }
 
-const loadConfig = function() {
-	if (!(config.get('ackee.token') || (config.get('ackee.username') && config.get('ackee.password')))) {
+const verifyAuth = () => {
+	const envToken = getEnv('ackee.token')
+	if (envToken !== undefined) {
+		config.set('ackee.token', envToken)
+		return
+	}
+
+	const configHasToken = config.get('ackee.token') !== undefined
+	const configHasCreds = config.get('ackee.username') !== undefined && config.get('ackee.password') !== undefined
+
+	if (configHasToken === false && configHasCreds === false) {
 		const token = prompt('ackee token (press enter to skip): ')
 		if (token.length < 1) {
 
@@ -34,6 +56,10 @@ const loadConfig = function() {
 			config.set('ackee.token', token)
 		}
 	}
+}
+
+const loadConfig = function() {
+	verifyAuth()
 
 	fields.forEach((field) => {
 		verifyField(field)
