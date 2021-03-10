a0f867b3cc2526a72714a7970575c52d4c8761ea
BetaHuhn <schiller@mxis.ch>
2021-03-10 19:49:16 +0100

    💡 Explain email port condition

--- src/service/email.js
+++ src/service/email.js
@@ -8,6 +8,7 @@ class Email {
 		const transporter = nodemailer.createTransport({
 			host: host,
 			port: port,
+			// Only start secure connection for port 465, for other ports use STARTTLS - reference: https://git.io/JqZPR
 			secure: port === '465',
 			auth: {
 				user: username,
